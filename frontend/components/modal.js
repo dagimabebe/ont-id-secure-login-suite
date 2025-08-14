af43c89292078ee172d8bb7a04bf8496af4dbc1f
dagim <166978716+dagimabebe@users.noreply.github.com>
2025-08-14 08:16:20 -0700

    Create modal.js

---
+++ frontend/components/modal.js
@@ -0,0 +1,8 @@
+
+export function showModal() {
+  document.getElementById('modal').style.display = 'flex';
+}
+
+export function hideModal() {
+  document.getElementById('modal').style.display = 'none';
+}
