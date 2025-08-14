5bb60bcc4223946c5873bab99a86439db4e639dc
dagim <166978716+dagimabebe@users.noreply.github.com>
2025-08-14 08:16:55 -0700

    Create spinner.js

---
+++ frontend/components/spinner.js
@@ -0,0 +1,8 @@
+
+export function showSpinner() {
+  document.getElementById('spinner').style.display = 'flex';
+}
+
+export function hideSpinner() {
+  document.getElementById('spinner').style.display = 'none';
+}
